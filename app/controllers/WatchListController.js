5a6fc5fb84c0457a4e496eb53dfffd76c09091c8
Sam Staff <samstaff@Sams-MacBook-Pro.local>
2017-08-09 14:27:21 -0500

    Watchlist functionality complete except for delete button

--- app/controllers/WatchListController.js
+++ app/controllers/WatchListController.js
@@ -4,36 +4,39 @@ app.controller("WatchListController", function($scope, $window, FBFactory, UserF
     $scope.search = () => {
         $window.location.href = '#!/search-view';
     };
-
-    let currentUser = UserFactory.getUser();
-
-   function displayWatchedRestaurants() {
+    let currentUser = null;
+    function displayWatchedRestaurants() {
         let restaurantArr = [];
         console.log("Display Watched called");
         FBFactory.getWatchList(currentUser)
-            .then((watchlist) => {
-                console.log("watchlist data", watchlist);
-                let watchData = watchlist.data;
-                Object.keys(watchData).forEach((key) => {
-                    watchData[key].id = key;
-                    restaurantArr.push(watchData[key]);
-                });
-
-                $scope.userWatch = restaurantArr;
-            })
-            .catch((err) => {
-                console.log("error in displayWatchedRestaurants in SearchController", err);
+        .then((watchlist) => {
+            console.log("watchlist data", watchlist);
+            let watchData = watchlist.data;
+            Object.keys(watchData).forEach((key) => {
+                watchData[key].id = key;
+                restaurantArr.push(watchData[key]);
             });
-    }
 
-    // $scope.remove((restaurant)=>{
-    //     console.log("delete called", restaurant);
-    //     FBFactory.deleteRestaurant(restaurant)
-    //         .then((data) => {
-    //             console.log("restaurant deleted", data);
-    //             displayWatchedRestaurants(currentUser);
-    //         });
-    // });
+            $scope.userWatch = restaurantArr;
+        })
+        .catch((err) => {
+            console.log("error in displayWatchedRestaurants in SearchController", err);
+        });
+    }
+    UserFactory.isAuthenticated()
+        .then((user) => {
+            console.log("user status", user);
+            currentUser = UserFactory.getUser();
+            displayWatchedRestaurants();
+        });
+    $scope.remove = (restaurant) => {
+        console.log("delete called", restaurant);
+        FBFactory.deleteRestaurant(restaurant)
+            .then((data) => {
+                console.log("restaurant deleted", data);
+                displayWatchedRestaurants(currentUser);
+            });
+    };
 
 
 
